3ed02c3eddb6c1fbf8eec5236b565b39549231c1
Konstantin-1x <kostia.haperskiy@mail.ru>
2024-11-07 01:54:38 +0300

    implementation of multiple subcategories

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,60 +15,51 @@ public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
         ParseWB parseWB = new ParseWB();
         List<String[]> tasks = Arrays.asList(
-                new String[]{"https://www.wildberries.ru/catalog/zhenshchinam", "Kantstovary.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/obuv", "obuv.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/detyam", "detyam.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/muzhchinam", "muzhchinam.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/dom-i-dacha", "dom-i-dacha.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/krasota", "krasota.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/aksessuary", "aksessuary.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/aksessuary/avtotovary", "avtotovary.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/elektronika", "elektronika.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/igrushki", "igrushki.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/dom/mebel", "mebel.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/aksessuary/tovary-dlya-vzroslyh", "tovary-dlya-vzroslyh.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/pitanie", "pitanie.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/bytovaya-tehnika", "bytovaya-tehnika.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/tovary-dlya-zhivotnyh", "tovary-dlya-zhivotnyh.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/sport", "sport.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/knigi", "knigi.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/yuvelirnye-ukrasheniya", "yuvelirnye-ukrasheniya.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/dom-i-dacha/instrumenty", "instrumenty.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/dachniy-sezon", "dachniy-sezon.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/dom-i-dacha/zdorove", "zdorove.txt"},
-                new String[]{"https://www.wildberries.ru/catalog/knigi-i-diski/kantstovary", "kantstovary.txt"}
+//                new String[]{"https://www.wildberries.ru/catalog/zhenshchinam", "TXTFILE/zhenshchinam.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/obuv", "TXTFILE/obuv.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/detyam", "TXTFILE/detyam.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/muzhchinam", "TXTFILE/muzhchinam.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/dom-i-dacha", "TXTFILE/dom-i-dacha.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/krasota", "TXTFILE/krasota.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/aksessuary", "TXTFILE/aksessuary.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/aksessuary/avtotovary", "TXTFILE/avtotovary.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/elektronika", "TXTFILE/elektronika.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/igrushki", "TXTFILE/igrushki.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/dom/mebel", "TXTFILE/mebel.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/aksessuary/tovary-dlya-vzroslyh", "tovary-dlya-vzroslyh.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/pitanie", "pitanie.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/bytovaya-tehnika", "bytovaya-tehnika.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/tovary-dlya-zhivotnyh", "tovary-dlya-zhivotnyh.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/sport", "sport.txt"}
+//                new String[]{"https://www.wildberries.ru/catalog/knigi", "knigi.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/yuvelirnye-ukrasheniya", "yuvelirnye-ukrasheniya.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/dom-i-dacha/instrumenty", "instrumenty.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/dachniy-sezon", "dachniy-sezon.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/dom-i-dacha/zdorove", "zdorove.txt"},
+//                new String[]{"https://www.wildberries.ru/catalog/knigi-i-diski/kantstovary", "kantstovary.txt"}
         );
+
+        parseWB.Parse("https://www.wildberries.ru/catalog/dom/mebel", "TXTFILE/mebel.txt");
         // Создаем пул из 3 потоков
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
-
-        // Добавляем задачи в очередь
-        for (String[] task : tasks) {
-            executorService.submit(() -> {
-                try {
-                    parseWB.Parse(task[0], task[1]);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-        }
-
-        // Завершаем выполнение пула потоков после завершения всех задач
-        executorService.shutdown();
-        while (!executorService.isTerminated()) {
-            // Ждем завершения всех задач
-        }
+//        ExecutorService executorService = Executors.newFixedThreadPool(3);
+//
+//        // Добавляем задачи в очередь
+//        for (String[] task : tasks) {
+//            executorService.submit(() -> {
+//                try {
+//                    parseWB.Parse(task[0], task[1]);
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//        }
+//
+//        // Завершаем выполнение пула потоков после завершения всех задач
+//        executorService.shutdown();
+//        while (!executorService.isTerminated()) {
+//            // Ждем завершения всех задач
+//        }
         System.out.println("All tasks completed, My Lord");
-
-//        WebDriver webDriver = new FirefoxDriver();
-//        BufferedWriter writer = new BufferedWriter(new FileWriter("items.txt",true));
-//        parseWB.Bypass(webDriver,  writer, "https://www.wildberries.ru/catalog/yuvelirnye-ukrasheniya/sergi",false);
-
-//        parseWB.Parse("https://www.wildberries.ru/catalog/knigi");
-//        WebDriver webDriver = new FirefoxDriver();
-//        webDriver.get("https://www.wildberries.ru/catalog/knigi-i-kantstovary/kantstovary/bumazhnaya-produktsiya");
-//        Thread.sleep(3000);
-//        List<WebElement> asd = webDriver.findElements(By.cssSelector("li.menu-category__subcategory-item"));
-//        System.out.println(asd.size());
     }
 }
 
--- src/main/java/org/example/ParseWB.java
+++ src/main/java/org/example/ParseWB.java
@@ -11,9 +11,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.IOException;
-
 public class ParseWB {
-    int Bypass(WebDriver webDriver, BufferedWriter writer,String hrefMenu, boolean subbol1, boolean subbol2) throws InterruptedException, IOException {
+    int TimeOut = 1500;
+    int Bypass(WebDriver webDriver, BufferedWriter writer,String hrefMenu, List<Boolean> subBool) throws InterruptedException, IOException {
         webDriver.manage().deleteAllCookies();
         Format format = new Format();
         int count = 0;
@@ -21,58 +21,51 @@ public class ParseWB {
         ((JavascriptExecutor) webDriver).executeScript("window.open('" + hrefMenu + "', '_blank');");
         String originalTab = webDriver.getWindowHandle();
         Set<String> allTabs = webDriver.getWindowHandles();
-        boolean subsubbol = subbol1;
         int skip = 0;
+        System.out.println("Я ломаюсь тут 1 ");
         for (String tab : allTabs) {
-            if(skip==2) {
-                subbol1 = false;
-                subbol2 = false;
+            if(skip==subBool.size()) {
+                System.out.println("Я ломаюсь тут 5 ");
+                subBool.replaceAll(ignored -> false);
             }
-            if(subbol1 && subbol2){
+            if(skip<subBool.size()){
+                System.out.println("Я ломаюсь тут 6 ");
                 skip++;
                 continue;
             }
-            if(subbol1 && !subbol2) {
-                subbol1 = false;
-                continue;
-            }
             if (!tab.equals(originalTab)) {
+                System.out.println("Я ломаюсь тут 4");
                 webDriver.switchTo().window(tab);
                 break;
             }
+            System.out.println("Я ломаюсь тут 3 ");
         }
-        Thread.sleep(1500);
+        System.out.println("Я ломаюсь тут 2 ");
+        Thread.sleep(TimeOut);
         List<WebElement> subcategiry = webDriver.findElements(By.cssSelector("a.menu-category__subcategory-link"));
         if(!subcategiry.isEmpty()){
+            subBool.add(true);
             for(WebElement subMenu : subcategiry){
-                if(subsubbol){
-                    String hrefSubMenu = subMenu.getAttribute("href");
-                    int ch = Bypass(webDriver,  writer, hrefSubMenu,true,true);
-                    if(ch == 404){
-                        System.out.println("All huinya, My Lord");
-                        continue;
-                    }
-                }else {
-                    String hrefSubMenu = subMenu.getAttribute("href");
-                    int ch = Bypass(webDriver,  writer, hrefSubMenu,true,false);
-                    if(ch == 404){
-                        System.out.println("All huinya, My Lord");
-                        continue;
-                    }
+                String hrefSubMenu = subMenu.getAttribute("href");
+                int ch = Bypass(webDriver,  writer, hrefSubMenu,subBool);
+                if(ch == 404){
+                    System.out.println("All huinya, My Lord");
+                    continue;
                 }
             }
             webDriver.close();
             webDriver.switchTo().window(originalTab);
             return 404;
         }
+
         //######################################################UNDERCATEGORIES###################################################
 
-        Thread.sleep(500);
+        Thread.sleep(3500);
         //######################################################FILTERS###################################################
         List<WebElement> filterButton = webDriver.findElements(By.cssSelector(".dropdown-filter__btn.dropdown-filter__btn--all"));
         while (filterButton.isEmpty()) {
             webDriver.navigate().refresh();
-            Thread.sleep(1500);
+            Thread.sleep(TimeOut);
             System.out.println("Strange huinya, My Lord");
             filterButton = webDriver.findElements(By.cssSelector(".dropdown-filter__btn.dropdown-filter__btn--all"));
         }
@@ -84,31 +77,57 @@ public class ParseWB {
             webDriver.switchTo().window(originalTab);
             return 404;
         }
-        WebElement filterRubliButton = filterContainer.get(0).findElement(By.tagName("button")); // Предполагается, что кнопка — это элемент <button>
-        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView({block: 'center', behavior: 'smooth'});", filterRubliButton);
+        List<WebElement> filterRubliButton = filterContainer.get(0).findElements(By.tagName("button"));
+        while (filterRubliButton.isEmpty()) {
+            webDriver.navigate().refresh();
+            Thread.sleep(TimeOut);
+            filterButton = webDriver.findElements(By.cssSelector(".dropdown-filter__btn.dropdown-filter__btn--all"));
+            while (filterButton.isEmpty()) {
+                webDriver.navigate().refresh();
+                Thread.sleep(TimeOut);
+                System.out.println("Strange huinya, My Lord");
+                filterButton = webDriver.findElements(By.cssSelector(".dropdown-filter__btn.dropdown-filter__btn--all"));
+            }
+            filterButton.get(0).click();
+            Thread.sleep(300);
+            filterContainer = webDriver.findElements(By.cssSelector(".filters-desktop__switch.j-filter-container.filters-desktop__switch--ffeedbackpoints.show"));
+            filterRubliButton = filterContainer.get(0).findElements(By.tagName("button"));
+        }
+        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView({block: 'center', behavior: 'smooth'});", filterRubliButton.get(0));
         Thread.sleep(300);
-        filterRubliButton.click();
+        filterRubliButton.get(0).click();
         Thread.sleep(300);
         List<WebElement> filterSubmitButton = webDriver.findElements(By.cssSelector(".filters-desktop__btn-main.btn-main"));
-        while(filterSubmitButton.isEmpty()){
-             filterContainer = webDriver.findElements(By.cssSelector(".filters-desktop__switch.j-filter-container.filters-desktop__switch--ffeedbackpoints.show"));
-            if(filterContainer.isEmpty()) {
-                webDriver.close();
-                webDriver.switchTo().window(originalTab);
-                return 404;
+        while(filterSubmitButton.isEmpty()) {
+            filterContainer = webDriver.findElements(By.cssSelector(".filters-desktop__switch.j-filter-container.filters-desktop__switch--ffeedbackpoints.show"));
+            filterRubliButton = filterContainer.get(0).findElements(By.tagName("button"));
+            while (filterRubliButton.isEmpty()) {
+                webDriver.navigate().refresh();
+                Thread.sleep(TimeOut);
+                filterButton = webDriver.findElements(By.cssSelector(".dropdown-filter__btn.dropdown-filter__btn--all"));
+                while (filterButton.isEmpty()) {
+                    webDriver.navigate().refresh();
+                    Thread.sleep(TimeOut);
+                    System.out.println("Strange huinya, My Lord");
+                    filterButton = webDriver.findElements(By.cssSelector(".dropdown-filter__btn.dropdown-filter__btn--all"));
+                }
+                filterButton.get(0).click();
+                Thread.sleep(300);
+                filterContainer = webDriver.findElements(By.cssSelector(".filters-desktop__switch.j-filter-container.filters-desktop__switch--ffeedbackpoints.show"));
+                filterRubliButton = filterContainer.get(0).findElements(By.tagName("button"));
             }
-            filterRubliButton = filterContainer.get(0).findElement(By.tagName("button")); // Предполагается, что кнопка — это элемент <button>
-            ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView({block: 'center', behavior: 'smooth'});", filterRubliButton);
+            ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView({block: 'center', behavior: 'smooth'});", filterRubliButton.get(0));
             Thread.sleep(300);
-            filterRubliButton.click();
+            filterRubliButton.get(0).click();
             Thread.sleep(300);
+            filterSubmitButton = webDriver.findElements(By.cssSelector(".filters-desktop__btn-main.btn-main"));
         }
         filterSubmitButton.get(0).click();
         Thread.sleep(200);
         List<WebElement> feedback = webDriver.findElements(By.className("feedbacks-points-sum"));
         while (feedback.isEmpty()) {
             webDriver.navigate().refresh();
-            Thread.sleep(1500);
+            Thread.sleep(TimeOut);
             System.out.println("Strange huinya, My Lord");
             feedback = webDriver.findElements(By.cssSelector(".feedbacks-points-sum"));
         }
@@ -120,8 +139,8 @@ public class ParseWB {
         List<WebElement> nextPage;
         do{
             for(int i=0;i<5;i++){
-                List<WebElement> Elemts = webDriver.findElements(By.cssSelector(".product-card.j-card-item"));
                 int schetchik = 0;
+                List<WebElement> Elemts = webDriver.findElements(By.cssSelector(".product-card.j-card-item"));
                 while (Elemts.isEmpty()){
                     if(schetchik>10) {
                         System.out.println("We are zaebalis' obnovlyat'sya, My Lord");
@@ -132,7 +151,7 @@ public class ParseWB {
                     schetchik++;
                     System.out.println("zaebalo obnovlyat'sya, My Lord");
                     webDriver.navigate().refresh();
-                    Thread.sleep(1500);
+                    Thread.sleep(TimeOut);
                     Elemts = webDriver.findElements(By.cssSelector(".product-card.j-card-item"));
                 }
                 WebElement lastElement = Elemts.get(Elemts.size() - 1);  // Получаем последний элемент из списка
@@ -169,7 +188,7 @@ public class ParseWB {
             }
             String nextHref = nextPage.get(0).getAttribute("href");
             webDriver.navigate().to(nextHref);
-            Thread.sleep(1500);
+            Thread.sleep(TimeOut);
         }while (!nextPage.isEmpty());
         //######################################################CHANGE-PAGES###################################################
         System.out.println(count);
@@ -179,18 +198,18 @@ public class ParseWB {
     }
     void Parse(String url,String fileName){
         WebDriver webDriver = new FirefoxDriver();
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName,true))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
 
             webDriver.get(url);
-            Thread.sleep(1500);
+            Thread.sleep(TimeOut);
             List<WebElement> GoToInMenus = webDriver.findElements(By.cssSelector(".menu-category__link"));
             for(WebElement Menu : GoToInMenus){
-
                 try {
                     //######################################################UNDERCATEGORIES###################################################
                     String hrefMenu;
                     hrefMenu = Menu.getAttribute("href");
-                    int ch = Bypass(webDriver,  writer, hrefMenu,false,false);
+                    List<Boolean> subBool = new ArrayList<>();
+                    int ch = Bypass(webDriver,  writer, hrefMenu,subBool);
                     if(ch == 404){
                         continue;
                     }
@@ -198,11 +217,9 @@ public class ParseWB {
                 System.err.println("Skip, My Lord");
             }
             }
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }finally {
+        } finally {
             System.out.println("All completed, My Lord");
             webDriver.quit();
         }
