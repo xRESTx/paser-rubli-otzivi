5b6e6377da28e06b08646cb04455b7cd8b83aa8c
Konstantin-1x <kostia.haperskiy@mail.ru>
2024-11-09 00:12:15 +0300

    add tgBot

---
+++ src/main/java/org/example/TgBot.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import com.pengrad.telegrambot.TelegramBot;
+
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+
+public class TgBot {
+    public TelegramBot bot;
+        public void sendMessage(String chatId, Integer messageThreadId, String messageText) {
+            boolean sent = false;
+            while (!sent) {
+                SendMessage request = new SendMessage(chatId, messageText);
+                if (messageThreadId != null) {
+                    request = request.replyToMessageId(messageThreadId);
+                }
+                SendResponse response = bot.execute(request);
+
+                if (response.isOk()) {
+                    System.out.println("Message sent successfully");
+                    sent = true;
+                } else {
+                    System.out.println("Failed to send message: " + response.errorCode() + " - " + response.description());
+                    int retryAfter = getRetryAfter(response);
+                    if (retryAfter > 0) {
+                        System.out.println("Too Many Requests: retry after " + retryAfter + " seconds");
+                        try {
+                            Thread.sleep(retryAfter * 1000L);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        private int getRetryAfter(SendResponse response) {
+            String description = response.description();
+            if (description != null && description.contains("retry after")) {
+                String[] parts = description.split(" ");
+                try {
+                    return Integer.parseInt(parts[parts.length - 1]);
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+            return 0;
+    }
+}
+
+
+
